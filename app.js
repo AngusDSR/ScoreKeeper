25dbe37c8204a7295d4314363fc3f1a3f03d46d4
Angus Spawton-Rice <adspawtonrice@gmail.com>
2023-07-17 20:53:06 +0100

    Initial commit//html, css & js files created

---
+++ app.js
@@ -0,0 +1,8 @@
+// console.log('Connected');
+
+const playerOneScoreSpan = document.getElementById('playerOneScore');
+const playerTwoScoreSpan = document.getElementById('playerTwoScore');
+
+// DEBUG
+console.log('playerOneScore: ', playerOneScore);
+console.log('playerTwoScore: ', playerTwoScore);
