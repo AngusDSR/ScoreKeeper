ec5974ddf980b5c7dd9775cbdccd1e88c05e2427
Angus Spawton-Rice <adspawtonrice@gmail.com>
2023-07-24 21:56:29 +0100

    scoring up to max score

--- app.js
+++ app.js
@@ -1,44 +1,48 @@
-// console.log('Connected');
-// select play to:
-// get item
-// on click display lsit
-// add hover effects
-// selecting changest the playTo value span and
-
+// Selecting the play to score
 const playToSelectionDiv = document.getElementById('dropdown-container-playTo')
 const playToButton = document.getElementById('playToSelectionButton')
 const playToSelection = document.getElementById('playToSelection')
 const PlayToOptions = document.querySelectorAll('.dropdown-selection-playTo')
+
+// Scoring
+const playerOneScoreSpan = document.getElementById('playerOneScore');
+const playerTwoScoreSpan = document.getElementById('playerTwoScore');
+const playerButtons = document.querySelectorAll('.playerButtons');
+const playerOneScore = 0;
+const playerTwoScore = 0;
 let PlayToScore = null;
 
+
+// Show/hide the play to score selection
 playToButton.addEventListener('click', (e) => {
   playToSelectionDiv.style.display = `${playToSelectionDiv.style.display != 'block' ? 'block': 'none' }`;
 })
 
+// Add listeners to each play to score option to allow selection
 PlayToOptions.forEach((option) => {
   option.addEventListener('click', () => {
     playerOneScore, playToSelection.innerText = parseInt(option.innerText,10);
     playToSelectionDiv.style.display = 'none';
     PlayToScore = parseInt(option.innerText,10);
-    console.log('PlayToScore: ', PlayToScore);
   })
 })
 
-
-const playerOneScoreSpan = document.getElementById('playerOneScore');
-const playerTwoScoreSpan = document.getElementById('playerTwoScore');
-
-// create function which adds ScreenOrientation
-// const raisePlayerScore (player) => {
-//
-// }
-
-// for each in [One, Two] add event listener to getElementById(`button-player${player}-score`)
-
-const playerOneScore = 0;
-document.getElementById('button-playerOne-score');
-
-// DEBUG
-// console.log('playerOneScoreSpan: ', playerOneScoreSpan);
-// console.log('playerTwoScoreSpan: ', playerTwoScoreSpan);
-// console.log('playToButton: ', playToButton);
+// Add listeners to each player button to allow scoring
+playerButtons.forEach((button) => {
+  button.addEventListener('click', (e) => {
+    if (PlayToScore === null) {
+      alert('⚠️ Please select a score to play to first.');
+      return;
+    } else if (e.target.id === 'button-playerOne-score') {
+      playerOneScoreSpan.innerText = parseInt(playerOneScoreSpan.innerText,10) + 1;
+    } else if (e.target.id === 'button-playerTwo-score') {
+      playerTwoScoreSpan.innerText = parseInt(playerTwoScoreSpan.innerText,10) + 1;
+    } if (parseInt(playerOneScoreSpan.innerText,10) === PlayToScore) {
+      playerOneScoreSpan.classList.add('winner');
+      playerTwoScoreSpan.classList.add('loser');
+      playerButtons.forEach((button) => {
+        button.disabled = true;
+      })
+    }
+  })
+})
