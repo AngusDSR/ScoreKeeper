2cd3e5bef992d745ea83a6451b93617ddc3cd05c
Angus Spawton-Rice <adspawtonrice@gmail.com>
2023-07-25 20:41:05 +0100

    Added selection // additional styling // tested

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 // Selecting the play to score
-const playToSelectionDiv = document.getElementById('dropdown-container-playTo')
-const playToButton = document.getElementById('playToSelectionButton')
-const playToSelection = document.getElementById('playToSelection')
-const PlayToOptions = document.querySelectorAll('.dropdown-selection-playTo')
+const playToSelectionDiv = document.getElementById('dropdown-container-playTo');
+const playToSelector = document.getElementById('playToSelector');
 
 // Scoring
 const playerOneScoreSpan = document.getElementById('playerOneScore');
@@ -11,89 +9,71 @@ const playerButtons = document.querySelectorAll('.playerButtons');
 const resetButton = document.getElementById('reset-button');
 let playerOneScore = 0;
 let playerTwoScore = 0;
-let PlayToScore = null;
+let playToScore = 0;
 
 // Winner's decoration
 const containerBody = document.querySelector('.container');
 
 // Show/hide the play to score selection
-playToButton.addEventListener('click', (e) => {
-  playToSelectionDiv.style.display = `${playToSelectionDiv.style.display != 'block' ? 'block': 'none' }`;
-})
-
-// Add listeners to each play to score option to allow selection
-PlayToOptions.forEach((option) => {
-  option.addEventListener('click', () => {
-    playToSelection.innerText = parseInt(option.innerText,10);
-    playToSelectionDiv.style.display = 'none';
-    PlayToScore = parseInt(option.innerText,10);
-  })
-})
+playToSelector.addEventListener('change', () => {
+  playToScore = parseInt(playToSelector.value, 10);
+});
 
 const addScore = (player) => {
   if (player === 1) {
-    playerOneScore += 1;
+    playerOneScore++;
     playerOneScoreSpan.innerText = playerOneScore;
   } else {
-    playerTwoScore += 1;
+    playerTwoScore++;
     playerTwoScoreSpan.innerText = playerTwoScore;
   }
-}
+};
 
 const showWinner = () => {
-  if (playerOneScore > playerTwoScore) {
-    containerBody.classList.add('player-1-wins');
-  } else {
-    containerBody.classList.add('player-2-wins');
-  }
-}
+  const winnerClass = playerOneScore > playerTwoScore ? 'player-1-wins' : 'player-2-wins';
+  containerBody.classList.add(winnerClass);
+};
 
 const checkScores = () => {
-  if (playerOneScore === PlayToScore || playerTwoScore === PlayToScore) {
+  if (playerOneScore === playToScore || playerTwoScore === playToScore) {
     playerButtons.forEach((button) => {
       button.disabled = true;
+      playToSelector.disabled = true;
       button.style.cursor = 'not-allowed';
       button.style.backgroundColor = 'grey';
-    })
+    });
     showWinner();
   }
-}
+};
 
 // Add listeners to each player button to allow scoring
 playerButtons.forEach((button) => {
   button.addEventListener('click', (e) => {
-    if (PlayToScore === null) {
+    if (playToScore === 0) {
       alert('⚠️ Please select a score to play to first.');
       return;
-    } else if (e.target.id === 'button-playerOne-score') {
-      addScore(1)
-    } else {
-      addScore(2)
     }
+    addScore(e.target.id === 'button-playerOne-score' ? 1 : 2);
     checkScores();
-  })
-})
-
+  });
+});
 
 // Reset the game
 const resetGame = () => {
-  playToSelection.innerText = "Select"
+  playToScore = 0;
   playerOneScore = 0;
   playerOneScoreSpan.innerText = playerOneScore;
   playerTwoScore = 0;
-  playerTwoScoreSpan.innerText = playerOneScore;
-  console.log(playerOneScore, playerOneScoreSpan.innerText, playerTwoScore, playerTwoScoreSpan.innerText);
-  containerBody.classList.remove('player-1-wins');
-  containerBody.classList.remove('player-2-wins');
+  playerTwoScoreSpan.innerText = playerTwoScore;
+  containerBody.classList.remove('player-1-wins', 'player-2-wins');
+  playToSelector.disabled = false;
+  console.dir(playToSelector);
+  playToSelector.selectedIndex = 0;
   playerButtons.forEach((button, i) => {
     button.disabled = false;
     button.style.cursor = 'pointer';
-    if (i === 0) {
-      button.style.backgroundColor = '#2D427A';
-    } else {
-      button.style.backgroundColor = '#d47d02';
-    }
-  })
-1}
+    button.style.backgroundColor = i === 0 ? '#2D427A' : '#d47d02';
+  });
+};
 
 resetButton.addEventListener('click', resetGame);
